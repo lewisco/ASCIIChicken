b9f12d417d05932bfd51e9c188b60849c41a49d3
lewisco <lewis@cobley.io>
2025-12-04 20:43:18 -0500

    feat: upgrade server to use ThreadingTCPServer for concurrent request handling.

--- server.py
+++ server.py
@@ -52,7 +52,7 @@ class ChickenHandler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(response_text.encode('utf-8'))
 
-with socketserver.TCPServer(("", PORT), ChickenHandler) as httpd:
+with socketserver.ThreadingTCPServer(("", PORT), ChickenHandler) as httpd:
     print(f"Serving ASCII Chicken at port {PORT}")
     try:
         httpd.serve_forever()
